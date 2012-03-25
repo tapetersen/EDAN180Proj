be42d99bdd70338cf48545106c45eca6cfaea484
Fredrik89 <fredrikjohnson89@gmail.com>
2012-03-25 21:52:58 +0200

    Integrated code-gen from labs (varför skriver vi commis på eng?)

---
+++ src/test/TestCodeGen.java
@@ -0,0 +1,45 @@
+package test;
+
+import icode.Code;
+
+import java.io.File;
+import java.io.FileReader;
+
+import org.junit.Test;
+
+import parser.MiniT;
+import semantic.Semantic;
+
+public class TestCodeGen extends TestCaseOutput {
+	private final static String DATA = "data/";
+	private final static String RESULT = "result/";
+	private final static String RESULT_EXTENSION = ".res";
+
+	private void assertCorrectOutput(String testName) {
+		String fullName = testName;
+		genCode(DATA + fullName);
+		assertOutput(new File(RESULT + fullName + RESULT_EXTENSION));
+	}
+
+	@Test
+	public void empty() {
+		assertCorrectOutput("empty");
+	}
+
+	private static void genCode(String arg) {
+		try {
+			Code code = new Code();
+			MiniT parser = new MiniT(new FileReader(arg));
+			Semantic s = new Semantic();
+			parser.start().genCode(code, s);
+			System.out.println(code);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+	}
+
+	public static void main(String[] args) {
+		genCode(args[0]);
+	}
+}
