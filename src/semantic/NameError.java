a1be6866402f2a859fde4c468eadeaca3c9627e8
Fredrik89 <fredrikjohnson89@gmail.com>
2012-03-29 17:17:48 +0200

    Paritally implemented linenumbered errors

--- src/semantic/NameError.java
+++ src/semantic/NameError.java
@@ -1,6 +1,15 @@
 package semantic;
 
 @SuppressWarnings("serial")
-public class NameError extends Exception {
-	public NameError(String message) { super(message); }
+public class NameError extends MiniTError {
+
+	public NameError(String message, int line) {
+		super(message, line);
+		// Auto-generated constructor stub
+	}
+
+	public NameError(String message) {
+		super(message, -1);
+	}
+
 }
--- src/semantic/TypeError.java
+++ src/semantic/TypeError.java
@@ -1,7 +1,14 @@
 package semantic;
 
 @SuppressWarnings("serial")
-public class TypeError extends Exception {
-	public TypeError(String message) { super(message); }
-}
+public class TypeError extends MiniTError {
 
+	public TypeError(String message, int line) {
+		super(message, line);
+		// Auto-generated constructor stub
+	}
+
+	public TypeError(String message) {
+		super(message, -1);
+	}
+}
