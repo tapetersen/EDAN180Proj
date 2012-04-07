a1bec3e62c3e2ea08feb8118038f13e58830ed82
Tobias Petersen <tobias.alex.petersen@gmail.com>
2012-04-07 03:10:50 +0200

    Implemented return checks compiles, who needs testing :P
    Should now complain about dead code due to early return statements and
    missing returns from functions with a type declared, Will add tests
    tomorrow.

--- src/semantic/MiniTError.java
+++ src/semantic/MiniTError.java
@@ -3,7 +3,7 @@ package semantic;
 public class MiniTError extends Exception {
 	private int line;
 	
-	MiniTError(String message, int line) {
+	public MiniTError(String message, int line) {
 		super(message);
 		this.line = line;
 	}
